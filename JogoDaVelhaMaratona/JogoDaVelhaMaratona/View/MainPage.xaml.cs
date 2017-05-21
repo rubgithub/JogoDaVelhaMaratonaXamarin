dd5b9cdbee2cb1f224ff10faeadf338c20d9abb8
rubgithub <rubensatual@hotmail.com>
2017-05-20 22:13:05 -0400

    Add project files.

---
+++ JogoDaVelhaMaratona/JogoDaVelhaMaratona/View/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace JogoDaVelhaMaratona.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
